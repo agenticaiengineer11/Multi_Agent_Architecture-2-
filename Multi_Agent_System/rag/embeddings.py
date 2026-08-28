6ceecffc204317030b499d363fdfd2059ce0524c
Muhammad Noman Ali <ranjhanoman75@gmail.com>
2026-08-28 20:45:27 +0500

    Docker file optimized

--- Multi_Agent_System/rag/embeddings.py
+++ Multi_Agent_System/rag/embeddings.py
@@ -1,8 +1,10 @@
+from functools import lru_cache
+
 from langchain_huggingface import HuggingFaceEmbeddings
 
 
+@lru_cache(maxsize=1)
 def create_embeddings():
-
     return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
